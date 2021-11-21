de2b8f7480e23e2427222982a80b8db93cd69832
jhosmillo <55266269+jhosmillo@users.noreply.github.com>
2021-11-21 12:38:31 -0800

    updating cart decisions if not logged in

--- js/placeOrder.js
+++ js/placeOrder.js
@@ -1,5 +1,9 @@
 function orderTicket(){
-	if(sessionStorage.getItem("Tokyo")!=null || sessionStorage.getItem("Kyoto")!=null || sessionStorage.getItem("Osaka")!=null){
+	if(sessionStorage.getItem("UserName")==null){
+		alert("Please login first");
+		event.preventDefault();
+	}	
+	if(sessionStorage.getItem("Tokyo")!=null || sessionStorage.getItem("Kyoto")!=null || sessionStorage.getItem("Osaka")!=null && sessionStorage.getItem("UserName")!=null){
 		let cusName = document.getElementById("firstName").value;
 		sessionStorage.setItem("CustomerName", cusName);
 		window.location.href = "thank-you.html";
