a942cdf4e6fa5c9b09706e185c2dff9aad160d25
jhosmillo <55266269+jhosmillo@users.noreply.github.com>
2021-11-21 17:49:15 -0800

    update contact form

---
+++ js/comment.js
@@ -0,0 +1,21 @@
+function processForm() {
+	// Prevent the form from submitting
+    event.preventDefault();
+	let userName = document.getElementById('name').value;
+	let userMsg = "Thanks " +userName+ ", well get back to you shortly."
+
+	//Set message
+	document.getElementById("message").innerHTML = userMsg;
+	document.getElementById("message").style.color = "white";
+
+	//disable input fields
+	document.getElementById('email').readOnly = true;
+	document.getElementById('name').readOnly = true;
+	document.getElementById('comment').readOnly = true;
+	document.getElementById('phone').readOnly = true;
+
+	
+	
+	
+}
+document.getElementById("submitButton").addEventListener("click", processForm);
--- js/placeOrder.js
+++ js/placeOrder.js
@@ -2,13 +2,13 @@ function orderTicket(){
 	if(sessionStorage.getItem("UserName")==null){
 		alert("Please login first");
 		event.preventDefault();
-	}	
-	if(sessionStorage.getItem("Tokyo")!=null || sessionStorage.getItem("Kyoto")!=null || sessionStorage.getItem("Osaka")!=null && sessionStorage.getItem("UserName")!=null){
+	}else if(sessionStorage.getItem("Tokyo")!=null || sessionStorage.getItem("Kyoto")!=null || sessionStorage.getItem("Osaka")!=null ){
 		let cusName = document.getElementById("firstName").value;
 		sessionStorage.setItem("CustomerName", cusName);
 		window.location.href = "thank-you.html";
 	}else{
 		alert("Your cart is empty!");
+		event.preventDefault();
 	}
 }
 
