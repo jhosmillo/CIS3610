22aeea10dbed638d6147d8992b3edb619758bda7
jhosmillo <55266269+jhosmillo@users.noreply.github.com>
2021-11-23 12:27:26 -0800

    Update placeOrder.js

--- js/placeOrder.js
+++ js/placeOrder.js
@@ -5,7 +5,7 @@ function orderTicket(){
 	}else if(sessionStorage.getItem("Tokyo")!=null || sessionStorage.getItem("Kyoto")!=null || sessionStorage.getItem("Osaka")!=null ){
 		let cusName = document.getElementById("firstName").value;
 		sessionStorage.setItem("CustomerName", cusName);
-		window.location.href = "thank-you.html";
+		//window.location.href = "thank-you.html";
 	}else{
 		alert("Your cart is empty!");
 		event.preventDefault();
